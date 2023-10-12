dd3127027e720538a3015027be30053f884ac9d0
Syam Pushpan <60807948+syam1245@users.noreply.github.com>
2023-10-12 13:44:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// // function hideDiv() {
+// //   var ele = document.getElementById('div-1');
+// //   ele.style.visibility = 'hidden';
+// // }
+
+// // var button = document.getElementById('btn-1');
+// // button.addEventListener('click', hideDiv);
+
+// function hideDiv() {
+//   document.getElementById("div-1").hidden = true;
+// }
+// function showDiv() {
+//   document.getElementById("div-1").hidden = false;
+// }
+// function showLastModifiedVal() {
+//   document.querySelector("#outputData").innerHTML = `last modified on: ${document.lastModified}`;
+// }
+
+
